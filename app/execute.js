6c7c297ffd2ca326b81d4f6d820c1b3d94e35d42
Salas, Claudio [CONBR] <csalas10@its.jnj.com>
2017-03-08 14:56:46 -0300

    adjusments and fixes

--- app/execute.js
+++ app/execute.js
@@ -3,18 +3,34 @@ const http = require('./http');
 const sonarqube = configuration.sonarqube;
 
 const execute = {};
+const opts = {
+	host: sonarqube.url
+}
+
+const backupPluginsList = function(path) {
+	opts.path = sonarqube.api.plugins.list;
+	http.get.json(opts)
+	.then(data => console.log(data));
+}
+
+const backupQualityGates = function(path) {
+	opts.path = sonarqube.api.qualitygates.list
+	http.get.json(opts).then(data => console.log(data));
+}
+
+const backupQualityProfiles = function(path) {
+	opts.path = sonarqube.api.qualityprofiles.list
+	http.get.json(opts).then(data => {
+		const urls = data.profiles.map(profile => `${sonarqube.api.qualityprofiles.backup}${profile.key}`)
+		console.log(urls);
+	});
+}
 
 execute.backup = function(path) {
 	console.log(`[execute][backup] - Backup will be saved at: ${path}`);
-	var opts = {
-		host: sonarqube.url,
-		path: sonarqube.api.plugins.list
-	};
-	http.get.json(opts).then(data => console.log(data));
-	opts.path = sonarqube.api.qualitygates.list
-	http.get.json(opts).then(data => console.log(data));
-	opts.path = sonarqube.api.qualityprofiles.list
-	http.get.json(opts).then(data => console.log(data));
+	backupQualityProfiles(path);	
 }
+
+
 
 module.exports = execute;
--- app/http.js
+++ app/http.js
@@ -7,10 +7,12 @@ const basicAuthorization = function() {
 	return 'Basic ' + new Buffer( `${sonarqube.api.key}:` ).toString('base64');
 }
 
-const formatOptions = function( { host = null, path = null } = {} ) {
+const formatOptions = function( { host = null, path = null, method = 'GET' } = {} ) {
 	return {
-		host: host,
+		url: host,
+		host: host.replace(/^(https?:|)\/\//,''),
 		path: path,
+		method: method,
 		headers: { 
 			Authorization:  basicAuthorization()
 		}
@@ -31,16 +33,17 @@ const httpRequest = function( { options = {}, toJSON = false } = {} ) {
 		}
 
 		// select http or https module, depending on reqested url
-		const http = options.host.startsWith('https') ? require('https') : require('http');
-
+		const http = options.url.startsWith('https') ? require('https') : require('http');
 		const request = http.request(options, (response) => {
 			const contentType = response.headers['content-type'];
 			// handle http errors
+		
 			if (response.statusCode < 200 || response.statusCode > 299) {
 				reject(new Error(`Failed to load page, status code: ${response.statusCode}`));
 			} else if (!/^application\/json/.test(contentType)) {
 	    		reject(new Error(`Invalid content-type.\nExpected application/json but received ${contentType}`));
 	    	}
+	    	
 			const rawData = [];
 			// on every content chunk, push it to the data array
 			response.on('data', (chunk) => rawData.push(chunk));
@@ -54,12 +57,13 @@ const httpRequest = function( { options = {}, toJSON = false } = {} ) {
 			});
 		});
 		// handle connection errors of the request
-		request.on('error', (err) => reject(err))
+		request.on('error', (err) => reject(err));
+		request.end();
 	});
 };
 
 methods.get.json = function( options ) {
-	return httpRequest( { options: formatOptions(options), toJSon: true } )
+	return httpRequest( { options: formatOptions(options), toJSON: true } )
 		.then(data => data)
 		.catch(err => err)
 };
